25058ab82c8007eb52c82bb90703bfefad53e2a7
Samuel BERNARD <samyvel80@gmail.com>
2023-03-26 14:07:12 +0200

    test qq API

---
+++ pizzamama/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from menu.serializers import PizzaSerializer
+from menu.models import Pizza
+
+
+from django.shortcuts import render
+
+# Create your views here.
