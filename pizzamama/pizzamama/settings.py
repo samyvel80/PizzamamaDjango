dbbfc2f2ac2098e1146939d1537796437151c94d
WinPascal <samyvel80@gmail.com>
2023-03-30 17:50:47 +0200

    container

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'pizzamama.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+##
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
