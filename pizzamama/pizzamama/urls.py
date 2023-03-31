d8cf556dbc658f370ef86ea7a44e488515854f2f
WinPascal <samyvel80@gmail.com>
2023-03-31 15:00:52 +0200

    config avec collectstatic

--- pizzamama/pizzamama/urls.py
+++ pizzamama/pizzamama/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+#for displaying static page import repath
 from django.urls import path, include,re_path
 from rest_framework.authtoken.views import obtain_auth_token
 ######for displaying static page collectstatic
